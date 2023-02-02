49f76af7630eb91ff2c769101a6b7bf854e89e40
maheshravuri <85814901+maheshravuri@users.noreply.github.com>
2023-02-02 11:22:26 +0530

    Create Time Conversion.java

---
+++ week-1/Time Conversion.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    String H = s.substring(0,2);    //hour
+    String M = s.substring(8,10);   //meridian
+    int h = Integer.parseInt(H);
+    if((h == 12 && M.equals("PM")) || (h<12 && M.equals("AM")))
+        return s.substring(0,8);
+    else if(h < 12 && M.equals("PM"))
+        return (12+h)+s.substring(2,8);
+    return "00"+s.substring(2,8);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
